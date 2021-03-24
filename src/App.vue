a7d045b0110c9a6b73cd4b60a2248903b90a341a
zeroo <anhtuan090795@gmail.com>
2021-03-24 15:33:32 +0700

    copy code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,90 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="product-category">
+    <Category :categories="categories" :subCategory="subCategory">
+    </Category>
+    <Products :products="products" :gallery="gallery"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Category from "./components/Category.vue";
+import Products from "./components/Products.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    Category,
+    Products,
+  },
+  data(){
+    return{
+      categories:[],
+      subCategory:[],
+      products:[],
+      gallery:[]
+    }
+  },
+  created(){
+    this.fetchCategory();
+    this.fetchProducts();
+  },
+  methods: {
+    fetchCategory(){
+      fetch("/categories.json")
+      .then(res => res.json())
+      .then(cate => {
+        if(cate.status === 200){
+          cate.result.map((c) =>{
+            this.categories.push({
+              id: c.id,
+              name: c.name,
+              url: c.url,
+              img_ulr: c.image_url,
+              sub: c.sub_category            
+            });
+            c.sub_category.map((sub) => {
+              this.subCategory.push({
+                id: sub.id,
+                parent_id: sub.parent_id,
+                name: sub.name,
+                url: sub.url,
+
+              })
+            })
+          })
+        }
+      })
+      .catch(err => alert(err))
+      console.log(this.categories);
+      console.log(this.subCategory);
+    },
+    fetchProducts(){
+      fetch("/products.json")
+      .then(res => res.json())
+      .then( product => {
+        if(product.status ===200){
+          product.result.map(p =>{
+            this.products.push({
+              id: p.id,
+              name: p.name,
+              url: p.url,
+              img: p.default_image_url
+            })
+            p.gallery.map(g => {
+              this.gallery.push({
+                url: g.image_url,
+                thumb: g.thumbnail_url,
+                img: g.image
+              });
+            });
+          });
+        }
+      })
+    .catch(err => alert(err));
+    console.log(this.products);
+    console.log( this.gallery);
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -22,5 +95,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+* {
+  margin: 0;
+  padding: 0;
+  font-family: sans-serif;
+  font-size: 16px;
+  color: #222;
+  box-sizing: border-box;
+}
+.product-category{
+  display: grid;
+  grid-template-columns: 15% 75%;
+  justify-content: center;
+}
+a {
+  text-decoration: none;
+  transition: all 0.3 linaer;
+}
+a:hover {
+  color:greenyellow;
 }
 </style>
---
+++ src/components/Category.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="category">
+    <ul class="menu">
+      <li><h3>Categories</h3></li>
+      <li
+        v-for="(cat, index) in categories"
+        :key="index"
+        :id="cat.id"
+      >
+        <a
+          href="#"
+          target="_blanck"
+          @click.prevent="addClass(index)"
+          :class="{actived: actived}"
+        >
+        {{cat.name}}
+          <ul v-if="cat_selected == cat.id"  class="sub_menu" style="margin-left: 20px;">
+            <li v-for="( scat, i ) in cat.sub" :key="i">
+              <a :href="scat.url"> {{scat.name}}</a>
+            </li>
+          </ul>
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      active: "",
+      actived: false,
+      cat_selected:'',
+      sub_menu: "sub-menu"
+    };
+  },
+  props: {
+    categories: Array,
+    subCategory: Array,
+  },
+  watch: {},
+  methods: {
+    addClass(index) {
+      this.cat_selected = this.categories[index].id;
+      this.actived = !this.actived;
+      console.log(this.active);
+    },
+  },
+};
+</script>
+<style scoped>
+.category .menu .sub-menu li {
+  margin-left: 15px;
+  display: none;
+}
+/* .category li  .sub-menu {
+    display: none;
+} */
+
+.category .menu  li a.actived  ul {
+  display: block;
+}
+.category .menu  li a  ul {
+ display: none;
+}
+
+ul {
+  text-align: left;
+  padding: 10px 0px;
+}
+ul li {
+  list-style: none;
+  padding: 10px 0;
+}
+a:hover {
+  color: greenyellow;
+}
+</style>
